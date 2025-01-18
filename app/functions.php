4bf18b089a572f34f2c74ce980cc5ffb6bb2ffa5
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-01-18 14:33:58 +0800

    Updated functions

--- app/functions.php
+++ app/functions.php
@@ -3,9 +3,6 @@
 /**
  * Includes a specific file using a relative path.
  * 
- * This function appends the provided relative path to the base path 
- * and includes the resulting file. It ensures the file is included 
- * only once during execution.
  * 
  * Example usage:
  * ```php
@@ -13,20 +10,17 @@
  * ```
  * 
  * @param string $path Relative path to the file to include, starting from the base directory.
+ * @param array $attributes [optional] An associative array of variables to be extracted and made available 
+ *                          within the scope of the included view (e.g., ['key' => 'value']). Defaults to an empty array.
  * @return void
  */
-function requireFile($path) {
+function requireFile($path, $attributes = []) {
+  extract($attributes);
   require_once BASE_PATH . $path;
 }
 
 /**
  * Includes a view file from the views directory with optional attributes.
- * 
- * This function appends the specified view path to the base directory path 
- * and includes the corresponding file. It ensures the file is included only once 
- * during the execution of the script. Additionally, it allows passing an associative 
- * array of attributes, which are extracted into individual variables for use within 
- * the included view.
  * 
  * Example usage:
  * ```php
@@ -43,6 +37,19 @@ function requireFile($path) {
 function requireView($viewPath, $attributes = []) {
   extract($attributes);
   require_once BASE_PATH . 'app/views/' . $viewPath;
+}
+
+/**
+ *  Includes a controller file from the views directory with optional attributes.
+ * @param string $controllerPath The relative path to the controller file, starting from the controller directory 
+ *                         (e.g., 'index.controller.php').
+ * @param array $attributes [optional] An associative array of variables to be extracted and made available 
+ *                          within the scope of the included view (e.g., ['key' => 'value']). Defaults to an empty array.
+ * @return void
+ */
+function requireController($controllerPath, $attributes = []) {
+  extract($attributes);
+  require_once BASE_PATH . 'app/controllers/' . $controllerPath;
 }
 
 
